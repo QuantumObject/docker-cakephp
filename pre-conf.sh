b77593b8b67e3ee7d3f7c61a7ab1a3184f74c42d
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-11 14:08:14 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
-ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
-
-cd /var/www/
- composer require cakephp/cakephp:"~3.1"
-
+ curl -o cakephp.zip -SL "https://github.com/cakephp/cakephp/archive/3.1.1.zip"
+ unzip cakephp.zip -d /var/www
+ 
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
