1638eb7477ad7686be0aa73fdffa5c68d34c7af1
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-19 21:00:41 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,7 +2,7 @@
 set -e
 
  cd /var/www
- curl -o cakephp.zip -SL "https://github.com/cakephp/cakephp/releases/download/3.1.3/cakephp-3-1-3.zip"
+ curl -o cakephp.zip -SL "https://github.com/cakephp/cakephp/releases/download/3.2.7/cakephp-3-2-7.zip"
  unzip cakephp.zip
  rm cakephp.zip
  
