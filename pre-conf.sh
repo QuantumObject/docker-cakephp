aa61d53b447d54c04d10e13225e67f686ceaaa7c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-01-16 13:32:03 -0500

    update to version 4.0.2
    update to version 4.0.2

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,7 +2,7 @@
 set -e
 
  cd /var/www
- curl -o cakephp.zip -SL "https://github.com/cakephp/cakephp/releases/download/3.6.11/cakephp-3-6-11.zip"
+ curl -o cakephp.zip -SL "https://github.com/cakephp/cakephp/releases/download/4.0.2/cakephp-4-0-2.zip"
  unzip cakephp.zip
  rm cakephp.zip
  
