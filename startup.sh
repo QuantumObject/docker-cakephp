6da2499ac6ab5d2c1a057ad35ae8d2955045cd88
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-01-16 15:06:24 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,10 +9,10 @@ else
         
         #if database present 
         if [ -n "$DB_NAME" ] ; then 
-           sed -i "s#'host' => 'localhost'\,#'host' => 'db'\,#"  /var/www/config/app.php
-           sed -i "s#'username' => 'my_app'\,#'username' => '`echo $DB_ENV_MYSQL_USER`'\,#"  /var/www/config/app.php
-           sed -i "s#'password' => 'secret'\,#'password' => '`echo $DB_ENV_MYSQL_PASSWORD`'\,#"  /var/www/config/app.php
-           sed -i "s#'database' => 'my_app'\,#'database' => '`echo $DB_ENV_MYSQL_DATABASE`'\,#"  /var/www/config/app.php
+           sed -i "s#'host' => 'localhost'\,#'host' => 'db'\,#"  /var/www/config/app_local.php
+           sed -i "s#'username' => 'my_app'\,#'username' => '`echo $DB_ENV_MYSQL_USER`'\,#"  /var/www/config/app_local.php
+           sed -i "s#'password' => 'secret'\,#'password' => '`echo $DB_ENV_MYSQL_PASSWORD`'\,#"  /var/www/config/app_local.php
+           sed -i "s#'database' => 'my_app'\,#'database' => '`echo $DB_ENV_MYSQL_DATABASE`'\,#"  /var/www/config/app_local.php
         fi
         #needed for fix problem with ubuntu and cron
         update-locale 
